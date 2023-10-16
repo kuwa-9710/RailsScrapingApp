1ebd976d173f6d96020bc06d8b85b6451eae5335
tomoya <shima.apple0941@gmail.com>
2023-10-16 14:34:47 +0900

    タイムゾーンを変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module RailsScrapingApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
+    # アプリケーションのタイムゾーンを日本に設定
+    config.time_zone = "Tokyo"
 
     # Configuration for the application, engines, and railties goes here.
     #
