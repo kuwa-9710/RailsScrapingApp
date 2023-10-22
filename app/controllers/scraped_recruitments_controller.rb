91c30c184e0bb7e01c8f9b6325d622d9fb1280ea
tomoya <shima.apple0941@gmail.com>
2023-10-22 16:17:48 +0900

    求人表示制限を50件に設定

--- app/controllers/scraped_recruitments_controller.rb
+++ app/controllers/scraped_recruitments_controller.rb
@@ -4,6 +4,6 @@ class ScrapedRecruitmentsController < ApplicationController
   def index
     RecruitmentCollector::Crawl.new.run
     RecruitmentCollector::Parse.new.run
-    @scraped_recruitments = ScrapedRecruitment.all.order(created_at: :desc)
+    @scraped_recruitments = ScrapedRecruitment.all.order(created_at: :desc).limit(50)
   end
 end
