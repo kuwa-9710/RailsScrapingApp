a8170969f7e55fc470bdcffcf37f5a62739d5cb8
tomoya <shima.apple0941@gmail.com>
2023-11-15 20:31:55 +0900

    scopeを使用

--- app/models/scraped_recruitment.rb
+++ app/models/scraped_recruitment.rb
@@ -1,5 +1,5 @@
 class ScrapedRecruitment < ApplicationRecord
-  def self.need_to_parse
-    ScrapedRecruitment.where(last_parsed_at: nil)
-  end
+  scope :need_to_parse, -> { where(last_parsed_at: nil) }
+
+  scope :parsed_contents, -> { order(created_at: :desc).limit(50) }
 end
