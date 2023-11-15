5e34391e5ff943d6558824d8805d329c214b8585
tomoya <shima.apple0941@gmail.com>
2023-11-15 13:44:39 +0900

    コンソール上でのみ動くように修正

--- app/controllers/scraped_recruitments_controller.rb
+++ app/controllers/scraped_recruitments_controller.rb
@@ -2,7 +2,6 @@ require_relative '../lib/recruitment_collector/crawl'
 
 class ScrapedRecruitmentsController < ApplicationController
   def index
-    CrawlAndParseJob.perform_later
     @scraped_recruitments = ScrapedRecruitment.all.order(created_at: :desc).limit(50)
   end
 end
