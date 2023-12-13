fab425100d84e7e5880bb2d2a4a7d5301fd7201b
kuwa-9710 <apple.941.s.j@gmail.com>
2023-12-13 23:28:02 +0900

    紐付け機能を修正

--- app/lib/recruitment_collector/associate.rb
+++ app/lib/recruitment_collector/associate.rb
@@ -15,28 +15,12 @@ module RecruitmentCollector
         next if organization.blank?
 
         # 紐付け情報を保存（既に過去紐付けを保存している場合はアップデート）
-        organizations_scraped_recruitment = OrganizationScrapedRecruitment.find_or_initialize_by(scraped_recruitment: recruitment)
-        organization_data = {
-          organization_name: organization.organization_name,
-          organization_email: organization.organization_email,
-          organization_phone_number: organization.organization_phone_number,
-          organization_hp: organization.organization_hp
-        }
-
-        organizations_scraped_recruitment.update(organization_data)
-
         need_to_update_recruitment_data = {
           last_confirmed_association_at: Time.zone.now,
           organization_id: organization.id
         }
         recruitment.update(need_to_update_recruitment_data)
       end
-    end
-
-    private
-
-    def find_or_create_organization_scraped_recruitment(recruitment)
-      
     end
   end
 end
