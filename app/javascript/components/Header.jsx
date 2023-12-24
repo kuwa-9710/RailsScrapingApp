cbfe69fe1b54ec4c7d0b2f99e89ec6ad09e8bfa0
kuwa-9710 <apple.941.s.j@gmail.com>
2023-12-24 20:31:17 +0900

    Reactを導入

---
+++ app/javascript/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="title__main">Scraping App</h1>
+    </header>
+  );
+};
+
+export default Header;
